4603819cf8debfeedd5933973940ce82808eb3da
Mideli <muzipovanastia@gmail.com>
2023-02-06 16:40:10 +0200

    feat: add greeting

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,19 +1,56 @@
-const time = document.querySelector('time')
-const dateInfo = document.querySelector('date')
+const time = document.querySelector('time');
+const dateInfo = document.querySelector('date');
 
 function showTime() {
-  const date = new Date()
-  let currentdate = date.toLocaleTimeString()
-  time.innerHTML = currentdate
-  showDate()
+  const date = new Date();
+  let currentdate = date.toLocaleTimeString();
+  time.innerHTML = currentdate;
+  showDate();
+  showGreetings();
   setTimeout(showTime, 1000);
 }
-showTime()
 
 function showDate() {
-  const date = new Date()
-  const options  = {weekday: 'long', month: 'long', day: 'numeric'};
-  let currentdate = date.toLocaleDateString('en-Br', options)
-  console.log(currentdate)
-  dateInfo.innerHTML = currentdate
+  const date = new Date();
+  const options = { weekday: 'long', month: 'long', day: 'numeric' };
+  let currentdate = date.toLocaleDateString('en-Br', options);
+  dateInfo.innerHTML = currentdate;
 }
+
+function getTimeOfDay() {
+  let date = new Date();
+  let hours = date.getHours();
+  let timeOfDay;
+  if (hours < 6) {
+    timeOfDay = 'Good night';
+  } else if (hours < 12) {
+    timeOfDay = 'Good morning';
+  } else if (hours < 18) {
+    timeOfDay = 'Good afternoon';
+  } else {
+    timeOfDay = 'Good evening';
+  }
+  return timeOfDay;
+}
+
+function showGreetings() {
+  const greeting = document.querySelector('.greeting-container span');
+  greeting.innerHTML = getTimeOfDay();
+}
+showTime();
+
+// ***********Greetings**************
+
+const name = document.querySelector('.name');
+
+function setLocalStorage() {
+  localStorage.setItem('name', name.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+  if (localStorage.getItem('name')) {
+    name.value = localStorage.getItem('name');
+  }
+}
+window.addEventListener('load', getLocalStorage);
